21e5d80551d252e310b90f137beda723034bb815
Andrew Lim <drewlim1994@gmail.com>
2017-07-13 23:00:00 +0800

    added customCell

---
+++ Swipesion/SavedNewsCell.swift
@@ -0,0 +1,26 @@
+//
+//  SavedNewsCell.swift
+//  Swipesion
+//
+//  Created by Andrew Lim on 13/07/2017.
+//  Copyright © 2017 Andrew Lim. All rights reserved.
+//
+
+import UIKit
+
+class SavedNewsCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
